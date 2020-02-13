663ece51ea1fa88cdf587df3cdfb34a5a1950048
guen9 <guen9@KH-PC>
2020-02-13 15:08:28 +0900

    Temp 정리

--- Portfolio_Huddling/src/main/java/com/kh/Portfolio_Huddling/maker/persistence/TempMakerBoardDao.java
+++ Portfolio_Huddling/src/main/java/com/kh/Portfolio_Huddling/maker/persistence/TempMakerBoardDao.java
@@ -1,7 +1,9 @@
 package com.kh.Portfolio_Huddling.maker.persistence;
 
+import com.kh.Portfolio_Huddling.maker.domain.TempMakerRequirDto;
+
 public interface TempMakerBoardDao {
 	
-	public void tempSaveRequir() throws Exception;
+	public void tempSaveRequir(TempMakerRequirDto makerDto) throws Exception;
 
 }
--- Portfolio_Huddling/src/main/java/com/kh/Portfolio_Huddling/maker/service/TempMakerBoardServiceImpl.java
+++ Portfolio_Huddling/src/main/java/com/kh/Portfolio_Huddling/maker/service/TempMakerBoardServiceImpl.java
@@ -1,8 +1,22 @@
 package com.kh.Portfolio_Huddling.maker.service;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Service;
+
+import com.kh.Portfolio_Huddling.maker.domain.TempMakerRequirDto;
+import com.kh.Portfolio_Huddling.maker.persistence.TempMakerBoardDao;
 
 @Service
 public class TempMakerBoardServiceImpl implements TempMakerBoardService {
+	
+	@Inject
+	private TempMakerBoardDao boardDao;
+
+	@Override
+	public void tempInsert(TempMakerRequirDto requirDto) throws Exception {
+		System.out.println(requirDto);
+		boardDao.tempSaveRequir(requirDto);
+	}
 
 }
