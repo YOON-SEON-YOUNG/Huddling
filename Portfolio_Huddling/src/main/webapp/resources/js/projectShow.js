d79487a2c25c9ae75c0801b77ef5fcfb8cec9f29
Jin <gudwls0645@naver.com>
2020-02-28 10:28:23 +0900

    메인, 회원가입, 로그인 로고 변경 클릭시 메인으로 이동, 관리자 승인/미승인 프로젝트명 클릭시 새창으로 띄움, 승인된 게시물만

--- Portfolio_Huddling/src/main/webapp/resources/js/projectShow.js
+++ Portfolio_Huddling/src/main/webapp/resources/js/projectShow.js
@@ -25,23 +25,25 @@
 
 			
 function arrStrPush(project_num, project_category, project_image, project_story, project_name, project_app) {
-		var strList = "";
-		strList +="<div style='height:350px;' class='col-md-4'>";
-		strList +="<a href='/detail/detailMain/"+encodeURI(project_num)+"'>";
-		strList +="<img style='height:250px; width:100%;' class='card-img-top' alt='Bootstrap Thumbnail First' src='/upload/imgView?fileName="+project_image+"'"; 
-		strList += "style='max-width: 100%; height: auto;'/>";
-		strList +="<div class='card-block'>";
-		strList +="<h5 class='card-title'>";
-		strList +="<br>";
-		strList +="<div style='height:20px;'>"+project_name+"</div>";
-		strList +="<br>";
-		strList +="</h5>";
-		strList +='<div class="progress">';
-		strList +='<div class="progress-bar" role="progressbar" style="width: 0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div></div>'
-		strList +="</div> </div> <p>";
-		strList +="</p> </div> </div> </a> </div>";
-		arrProject.push(strList);
-		percent(project_num);
+		if (project_app == 1) {
+			var strList = "";
+			strList +="<div style='height:350px;' class='col-md-4'>";
+			strList +="<a href='/detail/detailMain/"+encodeURI(project_num)+"'>";
+			strList +="<img style='height:250px; width:100%;' class='card-img-top' alt='Bootstrap Thumbnail First' src='/upload/imgView?fileName="+project_image+"'"; 
+			strList += "style='max-width: 100%; height: auto;'/>";
+			strList +="<div class='card-block'>";
+			strList +="<h5 class='card-title'>";
+			strList +="<br>";
+			strList +="<div style='height:20px;'>"+project_name+"</div>";
+			strList +="<br>";
+			strList +="</h5>";
+			strList +='<div class="progress">';
+			strList +='<div class="progress-bar" role="progressbar" style="width: 0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div></div>'
+			strList +="</div> </div> <p>";
+			strList +="</p> </div> </div> </a> </div>";
+			arrProject.push(strList);
+			percent(project_num);
+		}
 }
 
 // 더보기 처리 상단
